4d7d4e2165935f10477c999ea5805b18ead0c58b
abirbhab-dasgupta <codewithad24@gmail.com>
2024-06-13 17:24:18 +0530

    authentication added

--- auth.js
+++ auth.js
@@ -5,12 +5,12 @@ const Person = require('./models/Person')
 passport.use(new localStrategy(async(USERNAME,PASSWORD,done)=>{
     //authentication logic here 
     try{
-        console.log(`Recieved credentials:`,USERNAME,PASSWORD);
+        //console.log(`Recieved credentials:`,USERNAME,PASSWORD);
         const user = await Person.findOne({username:USERNAME});
         if (!user)
             return done(null,false,{message:'incorrect username'});
 
-        const isPasswordMatch = user.password === password ? true:false;
+        const isPasswordMatch = await user.comparePassword(password);
         if (isPasswordMatch)
             return done(null,user);
         else {
--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const personSchema = new mongoose.Schema({
     name: {
@@ -38,7 +39,37 @@ const personSchema = new mongoose.Schema({
         required:true
     }
 
+});
+
+personSchema.pre('save',async function(next){
+    const person = this;
+    if(person.isModified('password')) return next();
+    try{
+        //hash password generation 
+        const salt = await bcrypt.genSalt(10);
+
+        //hash password
+        const hashedPassword = await bcrypt.hash(person.password,salt);
+
+        //override the plaim password with the hashed password
+        person.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
 })
+
+personSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        //use bcryt to match the original password and hashed password 
+        const isMatch = await bcrypt.compare (candidatePassword,this.password);
+        return isMatch;
+
+    }catch(err){
+        throw err;
+
+    }
+}
 
 const Person = mongoose.model('Person',personSchema);
 module.exports = Person;
